5251aa2bae95451dbdd67a3e38dfdef7d02791ca
João Paulo <paulosantiago.cavalcante@gmail.com>
2022-09-16 00:30:44 -0300

    Atualizado App

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-
 app.register_blueprint(app_carro)
 app.register_blueprint(app_cliente)
 app.register_blueprint(app_empresa)
